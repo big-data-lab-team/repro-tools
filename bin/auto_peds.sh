8e895a62724649b13b22e1d9ddb330d2269dbce8
Ali Salari <32220327+ali4006@users.noreply.github.com>
2018-05-07 09:43:49 -0400

    Update auto_peds.sh

--- bin/auto_peds.sh
+++ bin/auto_peds.sh
@@ -1,16 +1,14 @@
 #!/bin/bash
 
-'''
-Pre-processing: after preparing the result of first condition (CentOS6)
-and getting the processes tree of pipeline using reprozip (db_sqlite), 
-we will detect the pipeline errors and the modify them in the following steps:
-(1) Running pipeline on the other condition (Centos7) to get the results
-(2) Running verify_files.py with results of two conditions to get the matrix file
-(3) Running 'peds.py' script based on the matrix file and db_sqlite 
-    to get a command_line list include all the claddified process
-(4) Modification step to fix the detected processes with error artificially
-(5) if command_line list is empty: break; else go step (1)
-'''
+#Pre-processing: after preparing the result of first condition (CentOS6)
+#and getting the processes tree of pipeline using reprozip (db_sqlite), 
+#we will detect the pipeline errors and the modify them in the following steps:
+#(1) Running pipeline on the other condition (Centos7) to get the results
+#(2) Running verify_files.py with results of two conditions to get the matrix file
+#(3) Running 'peds.py' script based on the matrix file and db_sqlite 
+#    to get a command_line list include all the claddified process
+#(4) Modification step to fix the detected processes with error artificially
+#(5) if command_line list is empty: break; else go step (1)
 
 # Start Modification Loop
 while [ -s /home/ubuntu/PEDS/test/peds_test/command_lines.txt ]
