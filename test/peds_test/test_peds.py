179310c9c484234818788a1620636d62a18fdf54
Ali Salari <32220327+ali4006@users.noreply.github.com>
2018-05-09 14:04:57 -0400

    Update test_peds.py

--- test/peds_test/test_peds.py
+++ test/peds_test/test_peds.py
@@ -1,7 +1,7 @@
 import pytest
 import commands
 import subprocess, os
-   
+
 def test_peds():
     command_line = "python ./reprotools/peds.py -db ./test/peds_test/trace.sqlite3 -ofile ./test/peds_test/error_matrix.txt"
     process = subprocess.Popen(command_line, shell=True,stdout=subprocess.PIPE, stderr=subprocess.PIPE)
@@ -9,13 +9,13 @@ def test_peds():
     assert(not code), "Command failed"
 
 def test_auto_peds():
-    command="python ./reprotools/auto_peds.py"
+    command="python ./reprotools/auto_peds.py test_script.sh input_file.txt ./test/peds_test/centos7/test/"
     process = subprocess.Popen(command, shell=True,stdout=subprocess.PIPE, stderr=subprocess.PIPE)
     output,error = process.communicate()
     if output:
-        print ("ret> ",process.returncode)
-        print ("OK> output ",output)
+        print "ret> ",process.returncode
+        print "OK> output ",output
     if error:
-        print ("ret> ",process.returncode)
-        print ("Error> error ",error.strip())
+        print "ret> ",process.returncode
+        print "Error> error ",error.strip()
     assert(open("./test/peds_test/total_commands.txt","r").read()==open("./test/peds_test/result_modif.txt","r").read())
