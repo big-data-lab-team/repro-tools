321d647cede9d9fd53fad378385c1d9a9114af46
Ali Salari <32220327+ali4006@users.noreply.github.com>
2018-05-09 13:02:20 -0400

    Update test_peds.py

--- test/peds_test/test_peds.py
+++ test/peds_test/test_peds.py
@@ -3,13 +3,13 @@ import commands
 import subprocess, os
    
 def test_peds():
-    command_line = "python reprotools/peds.py -db test/peds_test/trace.sqlite3 -ofile test/peds_test/error_matrix.txt"
+    command_line = "python ./reprotools/peds.py -db ./test/peds_test/trace.sqlite3 -ofile ./test/peds_test/error_matrix.txt"
     process = subprocess.Popen(command_line, shell=True,stdout=subprocess.PIPE, stderr=subprocess.PIPE)
     code=process.wait()
     assert(not code), "Command failed"
 
 def test_auto_peds():
-    command="python reprotools/auto_peds.py"
+    command="python ./reprotools/auto_peds.py"
     process = subprocess.Popen(command, shell=True,stdout=subprocess.PIPE, stderr=subprocess.PIPE)
     output,error = process.communicate()
     if output:
@@ -18,4 +18,4 @@ def test_auto_peds():
     if error:
         print "ret> ",process.returncode
         print "Error> error ",error.strip()
-    assert(open("test/peds_test/total_commands.txt","r").read()==open("test/peds_test/result_modif.txt","r").read())
+    assert(open("./test/peds_test/total_commands.txt","r").read()==open("./test/peds_test/result_modif.txt","r").read())
