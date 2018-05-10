e5c7ff3f4e120063df54611d04eb36220171ec18
Tristan Glatard <tristan.glatard@concordia.ca>
2018-05-10 10:37:10 -0400

    Linted file

--- test/test_verify_files.py
+++ test/test_verify_files.py
@@ -2,36 +2,51 @@ import os
 import pytest
 import commands
 import filecmp
-from reprotools.verifyFiles import get_dir_dict,read_metrics_file
-from reprotools.verifyFiles import checksum 
-from reprotools.verifyFiles import read_file_contents
-from reprotools.verifyFiles import get_conditions_dict,get_conditions_checksum_dict
+
+from reprotools.verify_files import get_dir_dict, read_metrics_file
+from reprotools.verify_files import checksum
+from reprotools.verify_files import read_file_contents
+from reprotools.verify_files import get_conditions_dict
+from reprotools.verify_files import get_conditions_checksum_dict
+
 
 def test_checksum():
-  assert checksum("test/condition4") == "45a021d9910102aac726dd222a898334"
+    assert checksum("test/condition4") == "45a021d9910102aac726dd222a898334"
+
 
 def test_dir_dict(tmpdir):
-  assert get_dir_dict("test/condition4","test/exclude_items.txt")
+    assert get_dir_dict("test/condition4", "test/exclude_items.txt")
+
 
 def test_conditions_dict():
-  conditions_dict = get_mock_conditions_dict()
-  assert conditions_dict['condition4'].keys()==conditions_dict['condition5'].keys()
+    conditions_dict = get_mock_conditions_dict()
+    assert(conditions_dict['condition4'].keys() ==
+           conditions_dict['condition5'].keys())
+
 
 def get_mock_conditions_dict():
-  conditions_list=read_file_contents("test/conditions.txt")
-  return get_conditions_dict(conditions_list,"test","test/exclude_items.txt") 
+    conditions_list = read_file_contents("test/conditions.txt")
+    return get_conditions_dict(conditions_list,
+                               "test",
+                               "test/exclude_items.txt")
+
 
 def test_conditions_checksum_dict():
-  conditions_dict = get_mock_conditions_dict()
-  assert get_conditions_checksum_dict(conditions_dict,"test","checksums-after.txt") 
+    conditions_dict = get_mock_conditions_dict()
+    assert(get_conditions_checksum_dict(conditions_dict,
+                                        "test",
+                                        "checksums-after.txt"))
+
 
 def test_run():
-  command_line_string ="python verifyFiles.py test/conditions.txt fileDiff  results -c checksums-after.txt -e test/exclude_items.txt"
-  return_value,output = commands.getstatusoutput(command_line_string)
-  assert not filecmp.cmp("results/fileDiff_differences_subject_total.txt","test/differences-ref.txt")
-  
+    command_line_string = ("python verifyFiles.py test/conditions.txt "
+                           "fileDiff  results -c checksums-after.txt -e"
+                           " test/exclude_items.txt")
+    return_value, output = commands.getstatusoutput(command_line_string)
+    assert not filecmp.cmp("results/fileDiff_differences_subject_total.txt",
+                           "test/differences-ref.txt")
+
 
 def test_read_metrics():
-  metrics=read_metrics_file("test/metrics-list.csv")
-  assert metrics["Filter Text"]["output_file"] == "test/filter.csv"
-    
+    metrics = read_metrics_file("test/metrics-list.csv")
+    assert metrics["Filter Text"]["output_file"] == "test/filter.csv"
