7a3629570419f8cd42a06cff6dc1a5d7f1bafe9b
ali4006 <ali.salar007@gmail.com>
2018-05-11 11:53:22 -0400

    update auto-peds script

--- reprotools/auto_peds.py
+++ reprotools/auto_peds.py
@@ -10,7 +10,7 @@ import os.path
 
 # Pre-processing: after preparing the result of first condition (CentOS6)
 # and getting the processes tree of pipeline using reprozip (db_sqlite),
-# we will detect the pipeline errors and the modify them in the following steps:
+# we detect the pipeline errors and the modify them in the following steps:
 # (1) Running pipeline on the other condition (Centos7) to get the results
 # (2) Running verify_files.py with results of two conditions to get the
 # matrix file
@@ -39,66 +39,94 @@ def which(exe=None):
     return None
 
 
-def replace_script(line, WD, WD_test):
+def bash_executor(self, command):
+    process = subprocess.Popen(command, shell=True, stderr=subprocess.PIPE)
+    output, error = process.communicate()
+    if error:
+        print("returnCode> ", process.returncode)
+        print("Error> ", error.strip())
+
+
+def replace_script(line, WD, peds_data_path):
     commands = str(line).split('##')[:1]
-    pipe_com = str(commands[0].replace('\x00' or ' ', ' '))
+    pipe_com = str(commands[0].replace('\x00', ' '))
     pipeline_commad = pipe_com.split(' ')
     pipe_cmd = pipeline_commad[0].split('/')[-1:][0]
-         # check the backup folder and then make cope from input_arg_cmd to that folder if doesn't exist
-    backup_path = WD_test+'/backup_scripts/'+str(pipe_cmd)
-    if os.path.exists(backup_path)==False:
-        bash_command = "cp " + str(which(pipeline_commad[0])) +" "+ backup_path
-        os.system(bash_command)
-        bash_command = "sudo cp " + WD+"/reprotools/make_copy.py " + str(which(pipeline_commad[0]))
-        print(bash_command)
-        os.system(bash_command)
-   ## instead of second copy make an alias : alias str(pipe_cmd)=$WD/reprotools/make_copy.py updated.txt common.txt"
-       # bash_command = "alias " + str(pipe_cmd)+"='python "+WD+"/reprotools/make_copy.py'"
-       # print(bash_command)
-#        subprocess.Popen(bash_command, shell=True)
+    # Make a copy of process to backup folder if doesn't exist
+    backup_path = os.path.join(peds_data_path, 'backup_scripts', str(pipe_cmd))
+    if os.path.exists(backup_path) is False:
+        cp_pipe_process = "cp " + str(which(pipeline_commad[0])) \
+                           + " " + backup_path
+        bash_executor(cp_pipe_process)
+        replace_command = "cp " + os.path.join(WD, 'reprotools/make_copy.py')\
+                          + ' ' + str(which(pipeline_commad[0]))
+        bash_executor(replace_command)
 
 
 def main(args=None):
 
     # Use argparse
+    parser = argparse.ArgumentParser(description='Automation of pipeline error detection')
+    parser.add_argument("-p", "--pipe_exec",
+                        help="executions file of the pipeline")
+    parser.add_argument("-i", "--pipe_input",
+                        help="input data of pipeline running")
+    parser.add_argument("-o", "--pipe_output",
+                        help="path of pipeline output directory")
+    parser.add_argument("-c", "--verify_condition",
+                        help="input the text file containing the path to the verify_file condition folders")
+    parser.add_argument("-r", "--verify_output",
+                        help="path of verify_file outputs")
+    parser.add_argument("-s", "--sqlite_db",
+                        help="sqlite file created by reprozip")
 
-    WD = os.environ['reprotool']
-    WD_test = os.environ['pedsfolder']
-    pipeline_script = sys.argv[1]
-    input_file = sys.argv[2]
-    output_folder = sys.argv[3]
-    print(pipeline_script)
-    print(output_folder)
-    print(input_file)
+    args = parser.parse_args()
+    # P = subprocess.Popen('pwd', shell=True, stderr=subprocess.PIPE)
+    # (WD, err) = P.communicate()
+    WD = os.getcwd()
+    peds_data_path = os.path.join(WD, "test/peds_test_data/")
+    pipe_exec = args.pipe_exec
+    pipe_input = args.pipe_input
+    pipe_output = args.pipe_output
+    verify_cond = args.verify_condition
+    verify_output = args.verify_output
+    sqlite_db = args.sqlite_db
 
 # Start Modification Loop
-    while True :
-#(1) Start the Pipeline execution
+    while True:
+# (1) Start the Pipeline execution
         # use popen instead of system. Pass the working directory to popen
-        pipeline_command = "(cd "+output_folder+"; sh "+pipeline_script+" "+input_file+")"
-        os.system(pipeline_command)
+        pipeline_command = "(cd "+pipe_output+"; sh "+pipe_exec+" "+pipe_input+")"
+        bash_executor(pipeline_command)
         # check the return code of popen
-#(2) Start to create the error matrix file
+# (2) Start to create the error matrix file
         # use os.path.join instead of '/'
         # pass conditions.txt as an argument
-        verify_command = 'verify_files '+ WD_test+'/conditions.txt test '+WD_test+'/result'
-        os.system(verify_command)
-        # do it in Python instead of awk
-        command = "awk '{print $1,$2}' "+ WD_test+"/result/test_differences_subject_total.txt | tail -n +2 >"+WD_test+"/error_matrix.txt"
-        os.system(command)
+        verify_command = 'verify_files ' + verify_cond + ' test ' + verify_output
+        bash_executor(verify_command)
 
-#(3) Start the classification
+        with open(os.path.join(verify_output, 'test_differences_subject_total.txt'), 'r') as mfile:
+            lines = mfile.readlines()
+        sample = ""
+        for line in lines[1:]:
+            splitedLine = line.split('\t')
+            sample += (splitedLine[0].replace(' ', '') + " " + str(int(splitedLine[1]))+"\n")
+        write_matrix = open(os.path.join(peds_data_path, 'error_matrix.txt'), 'w')
+        write_matrix.write(sample)
+
+# (3) Start the classification
         # call peds directly in Python instead of doing a system call
-        peds_command = "(cd "+WD_test+"; "+WD+'/reprotools/peds.py -db '+ WD_test+'/trace.sqlite3 -ofile '+WD_test+'/error_matrix.txt)'
-        os.system(peds_command)
+        peds_command = "(cd " + peds_data_path + "; peds -d " + sqlite_db + " -m error_matrix.txt)"
+        bash_executor(peds_command)
 
-#(4) Start the modification
-        if os.stat(WD_test+'/command_lines.txt').st_size>0:
-            with open(WD_test+'/command_lines.txt', 'r') as cfile:
+# (4) Start the modification
+        if os.stat(os.path.join(peds_data_path, 'command_lines.txt')).st_size > 0:
+            with open(os.path.join(peds_data_path, 'command_lines.txt'), 'r') as cfile:
                 lines = cfile.readlines()
                 for line in lines:
-                    replace_script(line, WD, WD_test)
+                    replace_script(line, WD, peds_data_path)
         else: break
 
+
 if __name__ == '__main__':
-      main()
+    main()
--- reprotools/make_copy.py
+++ reprotools/make_copy.py
@@ -12,12 +12,14 @@ import os.path
 #first, we replace the current script with the main pipeline process that create errors.
 #after that, script will make a copy of file if arguments are the same with backup command-lines.
 
+
 def is_intstring(s):
     try:
         float(s)
         return False
     except ValueError:
         return True
+
 
 def which(exe=None):
     '''
@@ -33,95 +35,95 @@ def which(exe=None):
                 return full_path
     return None
 
+
 def csv_parser(cfile):
-      command_parsed = {}
-      for line in cfile:
-         f_list = []
-         command = str(line).split('##')[:1]
-         command = str(command[0].replace('\x00' or ' ',' '))
-         file_name = str(line).split('##')[1:]
-         file_name = file_name[0][2:-3].replace("'",'')
-         file_name_list = file_name.split(',')
-         for file in file_name_list:
+    command_parsed = {}
+    for line in cfile:
+        f_list = []
+        command = str(line).split('##')[:1]
+        command = str(command[0].replace('\x00' or ' ', ' '))
+        file_name = str(line).split('##')[1:]
+        file_name = file_name[0][2:-3].replace("'", '')
+        file_name_list = file_name.split(',')
+        for file in file_name_list:
             count = 0
             for f in file.split('/'):
-           #    if f == "exec":
-               if f == "centos7":
-                  count +=1
-                  break
-               else: count +=1
+            #  if f == "exec":
+                if f == "centos7":
+                    count += 1
+                    break
+                else: count += 1
             f_list.append("/".join(file.split('/')[count:]))
-         command_parsed[command]=f_list
-      return command_parsed
+        command_parsed[command] = f_list
+    return command_parsed
+
 
 def main(args=None):
-
 ## set env: export pedsfolder=${PWD}/test/peds_test/
 ## set env: export reprotool=${PWD}
-   WD = os.environ['reprotool']
-   WD_test = os.environ['pedsfolder']
-   # updated commands refer to the single processes that create errors
-   # common_cmd refers to the multi-write processes that create errors
-   with open(WD_test+'/total_commands.txt', 'r') as cfile:
-       commands = csv_parser(cfile)
-   try:
-       with open(WD_test+'/common_cmd.txt', 'r')as multi_write_file:
-           multi_write_commands = csv_parser(multi_write_file)
-   except: multi_write_commands=[]
-   proc_list = multi_write_commands.keys()
+    WD = os.environ['reprotool']
+    WD_test = os.environ['pedsfolder']
+    # updated commands refer to the single processes that create errors
+    # common_cmd refers to the multi-write processes that create errors
+    with open(WD_test + '/total_commands.txt', 'r') as cfile:
+        commands = csv_parser(cfile)
+    try:
+        with open(WD_test + '/common_cmd.txt', 'r')as multi_write_file:
+            multi_write_commands = csv_parser(multi_write_file)
+    except: multi_write_commands = []
+    proc_list = multi_write_commands.keys()
 
-   input_arg_cmd = sys.argv[0]
-   current_script_name = __file__
-   cmd_name = current_script_name.split('/')[-1:][0]
-   command = WD_test+'/backup_scripts/'+str(cmd_name)
-   i = 1
-   while i<len(sys.argv):
-      command += " "+sys.argv[i]
-      i +=1
-   os.system(command) 
+    input_arg_cmd = sys.argv[0]
+    current_script_name = __file__
+    cmd_name = current_script_name.split('/')[-1:][0]
+    command = WD_test + '/backup_scripts/' + str(cmd_name)
+    i = 1
+    while i<len(sys.argv):
+        command += " " + sys.argv[i]
+        i += 1
+    subprocess.Popen(command, shell=True, stderr=subprocess.PIPE)
 
-   WD_ref = WD_test+"/centos6/test/"
-   WD_cur = WD_test+"/centos7/test/"
-   #WD_ref_saved = WD_test+"/centos6_common_files/"
-   #WD_cur_save = WD_test+"/centos7_common_files/"
-   for pipe_com, pipe_files in commands.items():
-      check = False
-      pipeline_commad = pipe_com.split(' ')
-      if str(which(pipeline_commad[0])) == str(which(input_arg_cmd)):
-         if len(pipeline_commad)-1 == len(sys.argv):
-            check = True
-            i = 1
-            while i < len(sys.argv):
-               if pipeline_commad[i] != sys.argv[i] and is_intstring(sys.argv[i]):
-                  check=False
-                  break
-               i +=1
-      if check==True:
-         for file in pipe_files:
-            from_path = WD_ref + file
-            To_path = WD_cur + file
-            my_bash_command = "cp " + from_path + " " + To_path
-            os.system(my_bash_command)
+    WD_ref = WD_test+"/centos6/test/"
+    WD_cur = WD_test+"/centos7/test/"
+    # WD_ref_saved = WD_test+"/centos6_common_files/"
+    # WD_cur_save = WD_test+"/centos7_common_files/"
+    for pipe_com, pipe_files in commands.items():
+        check = False
+        pipeline_commad = pipe_com.split(' ')
+        if str(which(pipeline_commad[0])) == str(which(input_arg_cmd)):
+            if len(pipeline_commad)-1 == len(sys.argv):
+                check = True
+                i = 1
+                while i < len(sys.argv):
+                    if pipeline_commad[i] != sys.argv[i] and is_intstring(sys.argv[i]):
+                        check = False
+                        break
+                    i += 1
+        if check is True:
+            for file in pipe_files:
+                from_path = WD_ref + file
+                To_path = WD_cur + file
+                cp_command = "cp " + from_path + " " + To_path
+                subprocess.Popen(cp_command, shell=True, stderr=subprocess.PIPE)
+                # f_ = file.split('/')[-1]
+                # _f = file.split('/')[:-1]
+                # if pipe_com in common_list:
+                #     proc_fname = str(pipeline_commad[0].split('/')[-1])+"_" #for common file add proc name:  'procname_filename'
+                #     #from_common_file = WD_ref + _f + proc_fname + f_
+                #     To_common_file = WD_cur + _f + proc_fname + f_ ##
+                #    # bash_command = "cp " + from_common_file + " " + To_common_file
+                #     bash_command = "cp " + from_path + " " + To_common_file
+                #     os.system(bash_command)
 
-            #f_ = file.split('/')[-1]
-            #_f = file.split('/')[:-1]
-            #if pipe_com in common_list:
-            #   proc_fname = str(pipeline_commad[0].split('/')[-1])+"_" #for common file add proc name:  'procname_filename'
-            #   #from_common_file = WD_ref + _f + proc_fname + f_
-            #   To_common_file = WD_cur + _f + proc_fname + f_ ##
-            #  # bash_command = "cp " + from_common_file + " " + To_common_file
-            #   bash_command = "cp " + from_path + " " + To_common_file
-            #   os.system(bash_command)
+                #if pipe_com in temp_list:
+                #   # from_temp_file = WD_ref + _f + "temp_" + f_ #for common file add temp:  'temp_filename'
+                #    To_temp_file = WD_cur + _f + "temp_" + f_
+                #   # bash_command = "cp " + from_temp_file + " " + To_temp_file
+                #    bash_command = "cp " + from_path + " " + To_temp_file
+                #    os.system(bash_command)
+            break
+        else: continue
 
-            #if pipe_com in temp_list:
-            #  # from_temp_file = WD_ref + _f + "temp_" + f_ #for common file add temp:  'temp_filename'
-            #   To_temp_file = WD_cur + _f + "temp_" + f_
-            #  # bash_command = "cp " + from_temp_file + " " + To_temp_file
-            #   bash_command = "cp " + from_path + " " + To_temp_file
-            #   os.system(bash_command)
-            
-         break
-      else: continue
 
 if __name__ == '__main__':
-      main()
+    main()
