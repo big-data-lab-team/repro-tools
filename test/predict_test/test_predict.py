7310538c9409e69796699e23770e4634a38e3086
Ali Salari <32220327+ali4006@users.noreply.github.com>
2018-09-06 11:49:39 -0400

    Update test_predict.py

--- test/predict_test/test_predict.py
+++ test/predict_test/test_predict.py
@@ -6,7 +6,6 @@ import sys
 
 #  shuffled_subject = [1,4,2,0,3]
 def test_run():
-    pyspark = pytest.importorskip("pyspark")
     command_line_string = ("predict test/predict_test/test_"
                            "differences.txt 0.6 ALS blah RFNT-S --seed_number 3")
     process = subprocess.Popen(command_line_string,
